8193ec2274781e94d0b01cb716c720451339301b
tburke9891 <thomasburke9891@gmail.com>
2026-02-28 11:19:31 -0500

    added player logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pygame
 import constants
+import player
+import pygame
+
 from logger import log_state
 
 def main():
@@ -7,6 +9,7 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0 # Delta time for frame rate independence
+    player1 = player.Player(x = constants.SCREEN_WIDTH / 2, y = constants.SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -18,6 +21,8 @@ def main():
         screen.fill("black")  # Clear the screen with black
         pygame.display.flip()  # Update the display
         dt = clock.tick(60) / 1000  # Limit to 60 frames per second
+        player1.draw(screen)  # Draw the player
+        player1.update(dt)  # Update the player's state
 
 if __name__ == "__main__":
     main()
